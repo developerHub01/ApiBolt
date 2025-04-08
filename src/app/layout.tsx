09de1412d74afa7790b8db34cc5d532f249d1626
developerHub01 <developerhubytchannel@gmail.com>
2025-04-08 22:58:27 +0600

    feat: changed chakra-ui to shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
-import { Provider } from "@/components/ui/provider";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "@/components/theme/theme-provider";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,8 +25,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Provider>{children}</Provider>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,11 @@
-import React from "react";
-import styles from "./page.module.css";
-
-import { Avatar, Button, Card } from "@chakra-ui/react";
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/mode-theme";
 
 const Home = () => {
   return (
     <div>
-      <Card.Root width="320px">
-        <Card.Body gap="2">
-          <Avatar.Root size="lg" shape="rounded">
-            <Avatar.Image src="https://picsum.photos/200/300" />
-            <Avatar.Fallback name="Nue Camp" />
-          </Avatar.Root>
-          <Card.Title mt="2">Nue Camp</Card.Title>
-          <Card.Description>
-            This is the card body. Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit. Curabitur nec odio vel dui euismod fermentum.
-            Curabitur nec odio vel dui euismod fermentum.
-          </Card.Description>
-        </Card.Body>
-        <Card.Footer justifyContent="flex-end">
-          <Button variant="outline">View</Button>
-          <Button>Join</Button>
-        </Card.Footer>
-      </Card.Root>
+      <ModeToggle />
+      <Button>Click me</Button>
     </div>
   );
 };
