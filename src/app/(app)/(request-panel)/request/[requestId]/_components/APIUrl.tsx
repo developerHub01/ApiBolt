066b6e8280270a2e0fa9a4cd6a6240ff4d6bc6ca
developerHub01 <developerhubytchannel@gmail.com>
2025-04-26 05:05:23 +0600

    feat: refactor APIUrl component to use memoization and enhance method selection with labels

--- src/app/(app)/(request-panel)/request/[requestId]/_components/APIUrl.tsx
+++ src/app/(app)/(request-panel)/request/[requestId]/_components/APIUrl.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React, { memo } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
@@ -9,21 +10,65 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import React from "react";
+import { useRequest } from "@/app/(app)/(request-panel)/request/[requestId]/_context/RequestProvider";
+import { cn } from "@/lib/utils";
 
-const methodList = ["get", "post", "patch", "put", "delete"];
+const methodList = [
+  {
+    id: "get",
+    label: "GET",
+  },
+  {
+    id: "post",
+    label: "POST",
+  },
+  {
+    id: "put",
+    label: "PUT",
+  },
+  {
+    id: "patch",
+    label: "PATCH",
+  },
+  {
+    id: "delete",
+    label: "DELETE",
+  },
+];
 
-const APIUrl = () => {
+const APIUrl = memo(() => {
+  const { selectedMethod, handleChangeSelectedMethod } = useRequest();
   return (
     <div className="w-full flex items-center">
-      <Select defaultValue={methodList[0]}>
-        <SelectTrigger className="w-[180px] rounded-r-none">
+      <Select
+        defaultValue={selectedMethod ?? methodList[0].id}
+        onValueChange={handleChangeSelectedMethod}
+      >
+        <SelectTrigger
+          className={cn("w-[150px] rounded-r-none font-semibold", {
+            "text-green-500": selectedMethod === "get",
+            "text-yellow-500": selectedMethod === "post",
+            "text-blue-500": selectedMethod === "put",
+            "text-pink-500": selectedMethod === "patch",
+            "text-red-500": selectedMethod === "delete",
+          })}
+        >
           <SelectValue placeholder="Method" />
         </SelectTrigger>
         <SelectContent>
-          {methodList.map((method) => (
-            <SelectItem key={method} value={method}>
-              {method.toUpperCase()}
+          {methodList.map(({ id, label }) => (
+            <SelectItem
+              key={id}
+              value={id}
+              className={cn("font-semibold", {
+                "text-green-500": id === "get",
+                "text-yellow-500": id === "post",
+                "text-blue-500": id === "put",
+                "text-pink-500": id === "patch",
+                "text-red-500": id === "delete",
+              })}
+            >
+              {label}
             </SelectItem>
           ))}
         </SelectContent>
@@ -32,6 +77,6 @@ const APIUrl = () => {
       <Button className="rounded-l-none">Send</Button>
     </div>
   );
-};
+});
 
 export default APIUrl;
