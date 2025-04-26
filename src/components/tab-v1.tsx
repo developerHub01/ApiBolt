65f70bfcd08ad41172b5ad97184e0d5ab08389e9
developerHub01 <developerhubytchannel@gmail.com>
2025-04-26 07:32:41 +0600

    feat: add TabV1 component for tab navigation with selectable buttons

---
+++ src/components/tab-v1.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+interface TabV1Props {
+  list: Array<{
+    id: string;
+    label: string;
+  }>;
+  activeTab: string;
+  handleSelect: (id: string) => void;
+}
+
+const TabV1 = ({ list, activeTab, handleSelect }: TabV1Props) => {
+  return (
+    <>
+      {list.map(({ id, label }) => (
+        <Button
+          key={id}
+          size={"sm"}
+          variant={"link"}
+          className={cn("px-0 underline-offset-8", {
+            "underline text-primary": activeTab === id,
+            "no-underline text-foreground/70 hover:text-foreground hover:no-underline":
+              activeTab !== id,
+          })}
+          onClick={() => handleSelect(id)}
+        >
+          {label}
+        </Button>
+      ))}
+    </>
+  );
+};
+
+export default TabV1;
