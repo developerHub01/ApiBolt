cffd5f9640aa12a79ee999573a586c6ec6952698
developerHub01 <developerhubytchannel@gmail.com>
2025-04-29 12:41:00 +0600

    feat: implement Headers component to display response headers in a table format

--- src/app/(app)/(request-panel)/request/[requestId]/_components/response/content/headers/Headers.tsx
+++ src/app/(app)/(request-panel)/request/[requestId]/_components/response/content/headers/Headers.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+"use client";
 
-const Headers = () => {
+import React, { memo, useMemo } from "react";
+import { useRequestResponse } from "@/app/(app)/(request-panel)/request/[requestId]/_context/RequestResponseProvider";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+const Headers = memo(() => {
+  const { response } = useRequestResponse();
+
+  const headers = useMemo(
+    () => Object.entries(response?.headers ?? {}),
+    [response]
+  );
+
   return (
-    <div>Headers</div>
-  )
-}
+    <ScrollArea className="w-full h-full">
+      <Table className="w-full h-full">
+        <TableHeader>
+          <TableRow>
+            <TableHead>Key</TableHead>
+            <TableHead>Value</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {headers.map(([key, value]) => (
+            <TableRow key={key}>
+              <TableCell className="capitalize">{key}</TableCell>
+              <TableCell>{value}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </ScrollArea>
+  );
+});
 
-export default Headers
+export default Headers;
