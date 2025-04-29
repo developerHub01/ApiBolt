7fe8a8b9eaca2e43e6994ed38ac6743c50b1a746
developerHub01 <developerhubytchannel@gmail.com>
2025-04-29 11:37:33 +0600

    feat: add SidebarProvider component for managing sidebar tab state

---
+++ src/app/(app)/_context/SidebarProvider.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import React, { createContext, useCallback, useContext, useState } from "react";
+
+export type TSidebarTab = "collections" | "environments" | null;
+
+interface SidebarContext {
+  activeTab: TSidebarTab;
+  lastActiveTab: TSidebarTab;
+  handleChangeActiveTab: (id?: TSidebarTab) => void;
+}
+
+const SidebarContext = createContext<SidebarContext | null>(null);
+
+export const useSidebar = () => {
+  const context = useContext(SidebarContext);
+
+  if (!context) {
+    throw new Error("useSidebar must be used within a SidebarProvider.");
+  }
+
+  return context;
+};
+
+interface SidebarProviderProps {
+  children: React.ReactNode;
+}
+
+const SidebarProvider = ({ children }: SidebarProviderProps) => {
+  const [activeTab, setActiveTab] = useState<TSidebarTab>("collections");
+  const [lastActiveTab, setLastActiveTab] =
+    useState<TSidebarTab>("collections");
+
+  const handleChangeActiveTab = useCallback(
+    (id: TSidebarTab = null) => {
+      setLastActiveTab(id);
+
+      if (id === activeTab) return setActiveTab(null);
+      setActiveTab(id);
+    },
+    [activeTab, setActiveTab]
+  );
+
+  return (
+    <SidebarContext.Provider
+      value={{
+        activeTab,
+        lastActiveTab,
+        handleChangeActiveTab,
+      }}
+    >
+      {children}
+    </SidebarContext.Provider>
+  );
+};
+
+export default SidebarProvider;
