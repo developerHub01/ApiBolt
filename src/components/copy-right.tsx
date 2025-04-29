7b990dfdc53203872f42ee15df5bdae00a06ed9a
developerHub01 <developerhubytchannel@gmail.com>
2025-04-29 11:36:50 +0600

    refactor: enhance CopyRight component with memoization and improved styling

--- src/components/copy-right.tsx
+++ src/components/copy-right.tsx
@@ -1,11 +1,24 @@
+"use client";
+
+import React, { memo, useCallback, useState } from "react";
+import { X as CloseIcon } from "lucide-react";
 import Link from "next/link";
-import React from "react";
+import useMounted from "@/hooks/use-mounted";
 
 const githubLink = "https://github.com/developerHub01";
 
-const CopyRight = () => {
+const CopyRight = memo(() => {
+  const [show, setShow] = useState<boolean>(true);
+  const mounted = useMounted();
+
+  const handleClick = useCallback(() => {
+    setShow(false);
+  }, []);
+
+  if (!mounted || !show) return null;
+
   return (
-    <div className="p-1 pt-1">
+    <div className="p-1 pt-1 bg-accent relative">
       <p className="text-center text-xs md:text-sm select-none">
         All rights reserve for
         <Link
@@ -16,8 +29,14 @@ const CopyRight = () => {
           Shakil
         </Link>
       </p>
+      <button
+        onClick={handleClick}
+        className="absolute top-1/2 right-1.5 -translate-y-1/2 p-0.5 cursor-pointer aspect-square rounded-full hover:bg-accent-foreground/20 transition-all duration-100"
+      >
+        <CloseIcon size={16} />
+      </button>
     </div>
   );
-};
+});
 
 export default CopyRight;
