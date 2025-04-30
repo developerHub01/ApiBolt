d7de58ea636edde9a98d29eec472022849f3cb86
developerHub01 <developerhubytchannel@gmail.com>
2025-04-30 06:32:02 +0600

    refactor: enhance table structure and styling in HeaderContent component

--- src/app/(app)/(request-panel)/request/[requestId]/_components/response/content/headers/HeaderContent.tsx
+++ src/app/(app)/(request-panel)/request/[requestId]/_components/response/content/headers/HeaderContent.tsx
@@ -16,14 +16,14 @@ interface HeaderContentProps {
 const HeaderContent = ({ headers }: HeaderContentProps) => {
   return (
     <ScrollArea className="w-full h-full">
-      <Table className="w-full h-full">
-        <TableHeader>
+      <Table className="w-full h-full border">
+        <TableHeader className="[&>tr>th]:border-r [&>tr>th]:last:border-r-0">
           <TableRow>
             <TableHead>Key</TableHead>
             <TableHead>Value</TableHead>
           </TableRow>
         </TableHeader>
-        <TableBody>
+        <TableBody className="[&>tr>td]:border-r [&>tr>td]:last:border-r-0">
           {Object.entries(headers).map(([key, value]) => (
             <TableRow key={key}>
               <TableCell className="capitalize">{key}</TableCell>
