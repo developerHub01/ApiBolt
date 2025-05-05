f573653040091bf14f7f1bedf889247eac8ff739
developerHub01 <developerhubytchannel@gmail.com>
2025-05-05 19:58:55 +0600

    refactor: header table for long text wrap text

--- src/app/(app)/(request-panel)/request/[requestId]/_components/response/content/headers/HeaderContent.tsx
+++ src/app/(app)/(request-panel)/request/[requestId]/_components/response/content/headers/HeaderContent.tsx
@@ -7,7 +7,6 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { ScrollArea } from "@/components/ui/scroll-area";
 
 interface HeaderContentProps {
   headers: Record<string, string>;
@@ -15,24 +14,24 @@ interface HeaderContentProps {
 
 const HeaderContent = ({ headers }: HeaderContentProps) => {
   return (
-    <ScrollArea className="w-full h-full">
-      <Table className="w-full h-full border">
-        <TableHeader className="[&>tr>th]:border-r [&>tr>th]:last:border-r-0">
-          <TableRow>
-            <TableHead>Key</TableHead>
-            <TableHead>Value</TableHead>
+    <Table className="w-full h-full border">
+      <TableHeader className="[&>tr>th]:border-r [&>tr>th]:last:border-r-0">
+        <TableRow>
+          <TableHead>Key</TableHead>
+          <TableHead>Value</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody className="[&>tr>td]:border-r [&>tr>td]:last:border-r-0">
+        {Object.entries(headers).map(([key, value]) => (
+          <TableRow key={key}>
+            <TableCell className="capitalize">{key}</TableCell>
+            <TableCell className="break-words whitespace-pre-wrap">
+              {value}
+            </TableCell>
           </TableRow>
-        </TableHeader>
-        <TableBody className="[&>tr>td]:border-r [&>tr>td]:last:border-r-0">
-          {Object.entries(headers).map(([key, value]) => (
-            <TableRow key={key}>
-              <TableCell className="capitalize">{key}</TableCell>
-              <TableCell>{value}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </ScrollArea>
+        ))}
+      </TableBody>
+    </Table>
   );
 };
 
