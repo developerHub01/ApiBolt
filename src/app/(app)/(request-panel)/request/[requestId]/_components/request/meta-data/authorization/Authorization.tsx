34cde7fc1bc6732f3a16ad591049b63104e79f41
developerHub01 <developerhubytchannel@gmail.com>
2025-05-07 15:58:35 +0600

    clear

--- src/app/(app)/(request-panel)/request/[requestId]/_components/request/meta-data/authorization/Authorization.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Authorization = () => {
-  return (
-    <div>Authorization</div>
-  )
-}
-
-export default Authorization
--- src/types/index.ts
+++
@@ -1,1 +0,0 @@
-export type TContentType = "text" | "html" | "xml" | "json" | "javascript";
