168babcb590a69597ef334b1f22cb13314875ddb
developerHub01 <developerhubytchannel@gmail.com>
2025-05-10 21:24:48 +0600

    feat: refactor CookieList component to dynamically generate table headers and rows based on cookie properties

--- src/ui/components/app/request-panel/request/response/content/cookies/CookieList.tsx
+++ src/ui/components/app/request-panel/request/response/content/cookies/CookieList.tsx
@@ -8,56 +8,49 @@ import {
 } from "@/components/ui/table";
 import type { CookieInterface } from "@/context/request/RequestResponseProvider";
 import DataTableCellContent from "@/components/app/request-panel/request/response/content/DataTableCellContent";
+import { useMemo } from "react";
 
 interface CookieListProps {
   cookies: Array<CookieInterface>;
 }
 
 const CookieList = ({ cookies }: CookieListProps) => {
+  const colList = useMemo(
+    () =>
+      Array.from(
+        cookies.reduce<Set<string>>((acc, curr) => {
+          return new Set([...acc, ...Object.keys(curr)]);
+        }, new Set())
+      ),
+    [cookies]
+  );
+
   return (
     <Table className="w-full h-full border">
       <TableHeader className="[&>tr>th]:border-r [&>tr>th]:last:border-r-0">
         <TableRow>
-          <TableHead>Name</TableHead>
-          <TableHead>Value</TableHead>
-          <TableHead>Domain</TableHead>
-          <TableHead>Path</TableHead>
-          <TableHead>Expires</TableHead>
-          <TableHead>HttpOnly</TableHead>
-          <TableHead>Secure</TableHead>
-          <TableHead>SameSite</TableHead>
+          {colList.map((item) => (
+            <TableHead key={item} className="capitalize">
+              {item}
+            </TableHead>
+          ))}
         </TableRow>
       </TableHeader>
       <TableBody className="[&>tr>td]:border-r [&>tr>td]:last:border-r-0">
-        {cookies.map(
-          ({
-            name = "",
-            value = "",
-            domain = "",
-            path = "",
-            expires = "",
-            HttpOnly = "",
-            secure = "",
-            samesite = "",
-          }) => (
-            <TableRow key={name} className="break-words whitespace-pre-wrap">
-              {[
-                name,
-                value,
-                domain,
-                path,
-                expires,
-                HttpOnly,
-                secure,
-                samesite,
-              ].map((value, index) => (
-                <TableCell key={index}>
-                  <DataTableCellContent value={value} />
-                </TableCell>
-              ))}
-            </TableRow>
-          )
-        )}
+        {cookies.map((cookie) => (
+          <TableRow
+            key={cookie.name}
+            className="break-words whitespace-pre-wrap"
+          >
+            {colList.map((item) => (
+              <TableCell key={item}>
+                <DataTableCellContent
+                  value={cookie[item as keyof typeof cookie] ?? ""}
+                />
+              </TableCell>
+            ))}
+          </TableRow>
+        ))}
       </TableBody>
     </Table>
   );
