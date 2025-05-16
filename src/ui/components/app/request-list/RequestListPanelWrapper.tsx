f23085dbe3de15dacdd640d5401c2526c020791e
developerHub01 <developerhubytchannel@gmail.com>
2025-05-17 00:50:06 +0600

    feat: add resizable functionality to RequestListPanelWrapper with collapse handling

--- src/ui/components/app/request-list/RequestListPanelWrapper.tsx
+++ src/ui/components/app/request-list/RequestListPanelWrapper.tsx
@@ -2,11 +2,23 @@ import { ResizablePanel } from "@/components/ui/resizable";
 import { useSidebar } from "@/context/sidebar/SidebarProvider";
 import RequestListPanel from "@/components/app/request-list/RequestListPanel";
 import RequestListProvider from "@/context/request-list/RequestListProvider";
+import { useRef } from "react";
+import type { ImperativePanelHandle } from "react-resizable-panels";
 
 const RequestListPanelWrapper = () => {
   const { activeTab } = useSidebar();
+  const resizableSizelRef = useRef<number | null>(null);
+  const resizablePanelRef = useRef<ImperativePanelHandle>(null);
 
   if (!activeTab) return null;
+
+  const handleResize = (value: number) => {
+    if (value < 20 && value < Number(resizableSizelRef.current)) {
+      resizablePanelRef.current?.collapse();
+    }
+
+    resizableSizelRef.current = value;
+  };
 
   return (
     <ResizablePanel
@@ -16,6 +28,8 @@ const RequestListPanelWrapper = () => {
       style={{
         maxWidth: "40vw",
       }}
+      onResize={handleResize}
+      ref={resizablePanelRef}
     >
       <RequestListProvider>
         <RequestListPanel />
