7a6cffa982245ac50b445e7f198ca6c88c2d8130
developerHub01 <developerhubytchannel@gmail.com>
2025-07-23 03:01:18 +0600

    feat: add lottie animations in auth, environments, projects

--- src/ui/components/app/authorization/content/no-auth/NoAuth.tsx
+++ src/ui/components/app/authorization/content/no-auth/NoAuth.tsx
@@ -1,31 +1,18 @@
 import { memo } from "react";
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
-import { motion } from "motion/react";
+import AnimationWrapper from "@/components/ui/AnimationWrapper";
 
 const NoAuth = memo(() => {
   return (
     <div className="text-center flex flex-col justify-center items-center gap-1.5 px-3 py-5 md:py-8">
-      <motion.div
-        className="w-52"
-        key="no-auth"
-        initial={{
-          scale: 0,
-        }}
-        animate={{
-          scale: 1,
-        }}
-        transition={{
-          duration: 0.5,
-          ease: "backOut",
-        }}
-      >
+      <AnimationWrapper className="w-52" key="no-auth">
         <DotLottieReact
           src={"./lottie/nodata.lottie"}
           loop
           autoplay
           width={200}
         />
-      </motion.div>
+      </AnimationWrapper>
       <h3 className="text-lg font-semibold">No Auth</h3>
       <p className="text-sm text-muted-foreground">
         This request does not use any authorization.
