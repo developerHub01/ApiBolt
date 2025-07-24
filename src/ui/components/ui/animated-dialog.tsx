40cfea28bf8a83a2d90bbf7bf4b2ce7ed4f243a8
developerHub01 <developerhubytchannel@gmail.com>
2025-07-24 20:28:39 +0600

    animated-dialog UI created

---
+++ src/ui/components/ui/animated-dialog.tsx
@@ -0,0 +1,166 @@
+import { AnimatePresence, motion } from "motion/react";
+import SettingProvider from "@/context/setting/SettingProvider";
+import { cn } from "@/lib/utils";
+import { memo } from "react";
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+interface AnimatedDialogProps {
+  isOpen?: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+  className?: string;
+  overlayClassName?: string;
+}
+
+const AnimatedDialog = memo(
+  ({
+    isOpen,
+    onClose,
+    children,
+    className = "",
+    overlayClassName = "",
+  }: AnimatedDialogProps) => {
+    return (
+      <SettingProvider>
+        <AnimatePresence>
+          {isOpen && (
+            <section
+              id="setting"
+              className={cn(
+                "absolute top-0 left-0 w-full h-full flex justify-center items-center p-10 z-50 overflow-hidden",
+                className
+              )}
+              onClick={onClose}
+            >
+              <motion.div
+                className={cn(
+                  "absolute top-0 left-0 w-full h-full bg-background/20 backdrop-blur-sm z-0",
+                  overlayClassName
+                )}
+                initial={{
+                  opacity: 0,
+                }}
+                animate={{
+                  opacity: 1,
+                }}
+                exit={{
+                  opacity: 0,
+                  transition: {
+                    delay: 0.5,
+                  },
+                }}
+                transition={{
+                  duration: 0.2,
+                  ease: "linear",
+                }}
+              ></motion.div>
+              {children}
+            </section>
+          )}
+        </AnimatePresence>
+      </SettingProvider>
+    );
+  }
+);
+
+interface AnimatedDialogContentWrapperProps {
+  className?: string;
+  children: React.ReactNode;
+}
+
+const AnimatedDialogContentWrapper = memo(
+  ({ className = "", children }: AnimatedDialogContentWrapperProps) => {
+    return (
+      <motion.section
+        className={cn(
+          "relative z-[1] w-full h-[95%] max-w-2xl bg-accent/50 backdrop-blur-lg rounded-lg flex flex-col overflow-hidden shadow-2xl",
+          className
+        )}
+        onClick={(e) => e.stopPropagation()}
+        initial={{
+          y: 50,
+          scale: 0.8,
+          opacity: 0,
+        }}
+        animate={{
+          y: 0,
+          scale: 1,
+          opacity: 1,
+          transition: {
+            delay: 0.2,
+          },
+        }}
+        exit={{
+          y: -20,
+          scale: 0.95,
+          opacity: 0,
+        }}
+        transition={{
+          duration: 0.3,
+          type: "spring",
+          ease: "anticipate",
+        }}
+      >
+        {children}
+      </motion.section>
+    );
+  }
+);
+
+interface AnimatedDialogTopProps {
+  className?: string;
+  children: React.ReactNode;
+}
+
+const AnimatedDialogTop = memo(
+  ({ className = "", children }: AnimatedDialogTopProps) => {
+    return <div className={cn("border-b-2 p-2.5", className)}>{children}</div>;
+  }
+);
+
+interface AnimatedDialogContentProps {
+  className?: string;
+  scrollAreaClassName?: string;
+  children: React.ReactNode;
+}
+
+const AnimatedDialogContent = memo(
+  ({
+    className = "",
+    scrollAreaClassName = "",
+    children,
+  }: AnimatedDialogContentProps) => {
+    return (
+      <div className={cn("w-full h-full min-h-0 flex-1", className)}>
+        <ScrollArea
+          className={cn("w-full h-full px-3 py-2", scrollAreaClassName)}
+        >
+          {children}
+        </ScrollArea>
+      </div>
+    );
+  }
+);
+
+const AnimatedDialogBottom = memo(
+  ({ className = "", children }: AnimatedDialogTopProps) => {
+    return (
+      <div
+        className={cn(
+          "border-t-2 p-2.5 flex justify-center items-center",
+          className
+        )}
+      >
+        {children}
+      </div>
+    );
+  }
+);
+
+export {
+  AnimatedDialog,
+  AnimatedDialogContentWrapper,
+  AnimatedDialogTop,
+  AnimatedDialogContent,
+  AnimatedDialogBottom,
+};
