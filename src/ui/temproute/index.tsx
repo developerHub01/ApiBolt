988282fdac310170ccdd42f7b83405db5ffc6d48
developerHub01 <developerhubytchannel@gmail.com>
2025-08-01 21:13:08 +0600

    folder names renamed into temp

--- src/ui/temproute/index.tsx
+++ src/ui/temproute/index.tsx
@@ -4,20 +4,20 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { isElectron } from "@/utils/electron";
-import RootLayout from "@/pages/layout";
-import AppLayout from "@/pages/app/layout";
-import FolderLayout from "@/pages/app/collections/folder/[id]/layout";
-import FolderPage from "@/pages/app/collections/folder/[id]/page";
-import RequestLayout from "@/pages/app/collections/request/[id]/layout";
-import RequestPage from "@/pages/app/collections/request/[id]/page";
-import EnvironmentLayout from "@/pages/app/environment/layout";
-import EnvironmentPage from "@/pages/app/environment/page";
-import ProjectsLayout from "@/pages/app/projects/layout";
-import ProjectsPage from "@/pages/app/projects/page";
-import AuthorizationLayout from "@/pages/app/authorization/layout";
-import AuthorizationPage from "@/pages/app/authorization/page";
-import CollectionsLayout from "@/pages/app/collections/layout";
-import CollectionPage from "@/pages/app/collections/page";
+import RootLayout from "@/temppages/layout";
+import AppLayout from "@/temppages/app/layout";
+import FolderLayout from "@/temppages/app/collections/folder/[id]/layout";
+import FolderPage from "@/temppages/app/collections/folder/[id]/page";
+import RequestLayout from "@/temppages/app/collections/request/[id]/layout";
+import RequestPage from "@/temppages/app/collections/request/[id]/page";
+import EnvironmentLayout from "@/temppages/app/environment/layout";
+import EnvironmentPage from "@/temppages/app/environment/page";
+import ProjectsLayout from "@/temppages/app/projects/layout";
+import ProjectsPage from "@/temppages/app/projects/page";
+import AuthorizationLayout from "@/temppages/app/authorization/layout";
+import AuthorizationPage from "@/temppages/app/authorization/page";
+import CollectionsLayout from "@/temppages/app/collections/layout";
+import CollectionPage from "@/temppages/app/collections/page";
 
 const routes = [
   {
