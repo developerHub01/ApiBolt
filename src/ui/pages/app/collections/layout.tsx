fdc15d2d4245a5cacda6274f0743e80160404836
developerHub01 <developerhubytchannel@gmail.com>
2025-08-24 02:20:25 +0600

    collection layout refactored

--- src/ui/pages/app/collections/layout.tsx
+++ src/ui/pages/app/collections/layout.tsx
@@ -1,12 +1,16 @@
 import AppMainContentLayoutWrapper from "@/components/app/AppMainContentLayoutWrapper";
-import LTRLayout from "@/components/app/collections/request/LTRLayout";
-import RTLLayout from "@/components/app/collections/request/RTLLayout";
 import RequestListPanelWrapper from "@/components/app/collections/request-list/RequestListPanelWrapper";
 import useCheckApplyingLayout from "@/hooks/setting/use-check-applying-layout";
 import { ResizableHandle } from "@/components/ui/resizable";
+import { Outlet, useParams } from "react-router-dom";
+import TabSidebar from "@/components/app/tab-sidebar/TabSidebar";
+import { ResizablePanel } from "@/components/ui/resizable";
+import LayoutMainPanel from "@/components/app/collections/request/LayoutMainPanel";
 import type { TLayoutSetting } from "@/types/setting.types";
+import { cn } from "@/lib/utils";
 
 const CollectionsLayout = () => {
+  const { id: requestId } = useParams<{ id?: string }>();
   const layoutTypes: TLayoutSetting = useCheckApplyingLayout();
 
   return (
@@ -17,7 +21,22 @@ const CollectionsLayout = () => {
           <ResizableHandle />
         </>
       )}
-      {layoutTypes === "ltr" ? <LTRLayout /> : <RTLLayout />}
+      <ResizablePanel defaultSize={70}>
+        <section
+          className={cn("flex w-full h-full flex-row", {
+            "flex-row-reverse": layoutTypes === "rtl",
+          })}
+        >
+          {requestId ? (
+            <>
+              <LayoutMainPanel />
+              <TabSidebar />
+            </>
+          ) : (
+            <Outlet />
+          )}
+        </section>
+      </ResizablePanel>
       {layoutTypes === "rtl" && (
         <>
           <ResizableHandle />
