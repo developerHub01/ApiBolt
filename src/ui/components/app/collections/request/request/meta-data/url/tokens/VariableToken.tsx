410ffa73f69cbbb2537e6bb33574493fcab1487f
developerHub01 <developerhubytchannel@gmail.com>
2025-09-08 00:49:25 +0600

    integrate EnvVariableSelector

--- src/ui/components/app/collections/request/request/meta-data/url/tokens/VariableToken.tsx
+++ src/ui/components/app/collections/request/request/meta-data/url/tokens/VariableToken.tsx
@@ -1,20 +1,6 @@
-import { memo, useMemo, useState } from "react";
-import { Check, ChevronsUpDown } from "lucide-react";
+import { memo, useCallback, useMemo } from "react";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-} from "@/components/ui/command";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
 import { useAppDispatch, useAppSelector } from "@/context/redux/hooks";
 import { selectEnvironmentsVariableList } from "@/context/redux/request-response/request-response-selector";
 import { Trash2 as DeleteIcon } from "lucide-react";
@@ -25,6 +11,7 @@ import {
   requestUrlDeleteToken,
   requestUrlUpdateToken,
 } from "@/context/redux/request-url/request-url-thunk";
+import EnvVariableSelector from "@/components/app/collections/request/request/EnvVariableSelector";
 
 interface VariableTokenProps {
   id: string;
@@ -33,18 +20,19 @@ interface VariableTokenProps {
 
 const VariableToken = memo(({ id, value }: VariableTokenProps) => {
   const dispatch = useAppDispatch();
-  const [open, setOpen] = useState<boolean>(false);
   const variableList = useAppSelector(selectEnvironmentsVariableList);
 
-  const handleChangeVariable = (variable: string) => {
-    dispatch(
-      requestUrlUpdateToken({
-        id,
-        value: variable === value ? "" : variable,
-      })
-    );
-    setOpen(false);
-  };
+  const handleChangeVariable = useCallback(
+    (variable: string) => {
+      dispatch(
+        requestUrlUpdateToken({
+          id,
+          value: variable === value ? "" : variable,
+        })
+      );
+    },
+    [dispatch, id, value]
+  );
 
   const handleDelete = () => {
     dispatch(requestUrlDeleteToken(id));
@@ -59,78 +47,30 @@ const VariableToken = memo(({ id, value }: VariableTokenProps) => {
   const isNotExist = value && !isVariableExistInList;
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <ButtonLikeDiv
-        className={cn(
-          "w-fit min-w-40 flex gap-0 p-0 rounded-md bg-transparent hover:bg-transparent ring",
-          {
-            "ring-green-500/60": isExist,
-            "ring-red-500/60": isNotExist,
-          }
-        )}
+    <ButtonLikeDiv
+      className={cn(
+        "w-fit min-w-40 flex gap-0 p-0 rounded-md bg-transparent hover:bg-transparent ring",
+        {
+          "ring-green-500/60": isExist,
+          "ring-red-500/60": isNotExist,
+        }
+      )}
+    >
+      <TokenDragHandler />
+      <EnvVariableSelector
+        value={value}
+        onChange={handleChangeVariable}
+        className="flex-1 justify-between rounded-none h-full max-w-96"
+      />
+      <ChangeTokenType id={id} type={"env"} />
+      <Button
+        variant={"secondary"}
+        className="rounded-l-none h-full min-w-20"
+        onClick={handleDelete}
       >
-        <TokenDragHandler />
-        <PopoverTrigger asChild>
-          <Button
-            variant="secondary"
-            role="combobox"
-            aria-expanded={open}
-            className={cn("flex-1 justify-between rounded-none", {
-              "text-green-500/60": isExist,
-              "text-red-500/60": isNotExist,
-            })}
-          >
-            <p className="flex-1 overflow-hidden text-left">
-              {value
-                ? (variableList.find(
-                    (framework) => framework.variable === value
-                  )?.variable ?? value)
-                : "Select variable..."}
-            </p>
-            <ChevronsUpDown className="opacity-50" />
-          </Button>
-        </PopoverTrigger>
-        <ChangeTokenType id={id} type={"env"} />
-        <Button
-          variant={"secondary"}
-          className="rounded-l-none"
-          onClick={handleDelete}
-        >
-          <DeleteIcon />
-        </Button>
-      </ButtonLikeDiv>
-      <PopoverContent
-        className="w-[200px] p-0"
-        side="bottom"
-        align="start"
-        sideOffset={8}
-      >
-        <Command>
-          <CommandInput placeholder="Search variable..." className="h-9" />
-          <CommandList>
-            <CommandEmpty>No variable found.</CommandEmpty>
-            <CommandGroup>
-              {variableList.map((variable) => (
-                <CommandItem
-                  key={variable.variable}
-                  value={variable.variable}
-                  onSelect={handleChangeVariable}
-                  className="w-full"
-                >
-                  <p className="flex-1 overflow-hidden">{variable.variable}</p>
-                  <Check
-                    className={cn(
-                      "ml-auto",
-                      value === variable.variable ? "opacity-100" : "opacity-0"
-                    )}
-                  />
-                </CommandItem>
-              ))}
-            </CommandGroup>
-          </CommandList>
-        </Command>
-      </PopoverContent>
-    </Popover>
+        <DeleteIcon />
+      </Button>
+    </ButtonLikeDiv>
   );
 });
 
