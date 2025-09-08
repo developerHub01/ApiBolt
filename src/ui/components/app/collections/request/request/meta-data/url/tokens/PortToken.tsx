38899b364ba947721c427da063e3f333f1f09084
developerHub01 <developerhubytchannel@gmail.com>
2025-09-08 20:20:26 +0600

    PortToken validation added

--- src/ui/components/app/collections/request/request/meta-data/url/tokens/PortToken.tsx
+++ src/ui/components/app/collections/request/request/meta-data/url/tokens/PortToken.tsx
@@ -4,44 +4,80 @@ import {
   useState,
   type ChangeEvent,
   type FocusEvent,
+  type KeyboardEvent,
 } from "react";
 import { ButtonLikeDiv } from "@/components/ui/button-like-div";
 import { useAppDispatch, useAppSelector } from "@/context/redux/hooks";
 import { selectRequestUrlTokenPort } from "@/context/redux/request-url/request-url-selector";
 import { requestUrlUpdateOriginToken } from "@/context/redux/request-url/request-url-thunk";
+import { isValidPort } from "@/utils/request-url.utils";
+import { cn } from "@/lib/utils";
+
+const isInvalidPort = (port: string): boolean => !isValidPort(port);
+const allowedKeys = new Set([
+  "Backspace",
+  "Delete",
+  "ArrowLeft",
+  "ArrowRight",
+  "Tab",
+  "Enter",
+]);
 
 const PortToken = memo(() => {
   const dispatch = useAppDispatch();
   const port = useAppSelector(selectRequestUrlTokenPort);
   const [portState, setPortState] = useState<string>(port);
+  const [isError, setIsError] = useState<boolean>(false);
 
   useEffect(() => {
     if (port === portState) return;
     setPortState(port);
+    setIsError(isInvalidPort(port));
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [port]);
 
-  const handleChangePort = (e: ChangeEvent<HTMLInputElement>) =>
-    setPortState(e.target.value);
+  const handleChangePort = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setPortState(value);
+    setIsError(isInvalidPort(value));
+  };
 
-  const handleBlurPort = (e: FocusEvent<HTMLInputElement>) => {
-    if (e.target.value === port) return;
+  const handleBlurPort = (e: FocusEvent<HTMLInputElement>) =>
+    handleUpdate(e.target.value);
+
+  const handleKeydown = (e: KeyboardEvent<HTMLHeadingElement>) => {
+    if (["Enter"].includes(e.key)) return handleUpdate(portState);
+    if (allowedKeys.has(e.key)) return;
+    // block non-numeric
+    if (!/^[0-9]$/.test(e.key)) return e.preventDefault();
+  };
+
+  const handleUpdate = (value: string) => {
+    const haveError = isInvalidPort(value);
+    setIsError(haveError);
+    if (value === port || haveError) return;
 
     dispatch(
       requestUrlUpdateOriginToken({
         id: "port",
-        value: portState,
+        value,
       })
     );
   };
 
   return (
-    <ButtonLikeDiv variant={"secondary"} className="w-28">
+    <ButtonLikeDiv
+      variant={"secondary"}
+      className={cn("w-28 ring ring-transparent", {
+        "ring-red-500/60": isError,
+      })}
+    >
       <input
         placeholder="Port eg. 3000"
         className="w-full border-0 border-b font-normal"
         value={portState}
         onChange={handleChangePort}
+        onKeyDown={handleKeydown}
         onBlur={handleBlurPort}
       />
     </ButtonLikeDiv>
