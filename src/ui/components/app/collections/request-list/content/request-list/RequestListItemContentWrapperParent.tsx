4ce0affe5b1fe2e3a004a08dfa71f793eb845479
developerHub01 <developerhubytchannel@gmail.com>
2025-09-11 02:30:32 +0600

    bug fix: clicking on request list item was not responding issue solved. other props was not passed in the RequestListItemContentWrapperParent that passed now

--- src/ui/components/app/collections/request-list/content/request-list/RequestListItemContentWrapperParent.tsx
+++ src/ui/components/app/collections/request-list/content/request-list/RequestListItemContentWrapperParent.tsx
@@ -7,13 +7,18 @@ interface Props {
 }
 
 const RequestListItemContentWrapperParent = memo(
-  ({ className = "", children }: Props & React.ComponentProps<"div">) => {
+  ({
+    className = "",
+    children,
+    ...props
+  }: Props & React.ComponentProps<"div">) => {
     return (
       <div
         className={cn(
           "w-full cursor-pointer hover:bg-accent/50 focus-within:bg-accent/50 duration-100 transition-all px-1 border-x-2 h-9 border-transparent",
           className
         )}
+        {...props}
       >
         {children}
       </div>
