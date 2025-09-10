8a651254942c0de164d5309cfaa8e209f56c0a0d
developerHub01 <developerhubytchannel@gmail.com>
2025-09-10 23:31:39 +0600

    RequestListItemExpendedContent empty folder space and time line related bug fixed

--- src/ui/components/app/collections/request-list/content/request-list/RequestListItemExpendedContent.tsx
+++ src/ui/components/app/collections/request-list/content/request-list/RequestListItemExpendedContent.tsx
@@ -1,11 +1,6 @@
-import { useCallback } from "react";
 import RequestListItem from "@/components/app/collections/request-list/content/request-list/RequestListItem";
-import { Button } from "@/components/ui/button";
-import { useAppDispatch } from "@/context/redux/hooks";
-import { createSingleRequest } from "@/context/redux/request-response/thunks/request-list";
 import type { RequestListItemInterface } from "@/types/request-response.types";
-import RequestListItemLine from "@/components/app/collections/request-list/content/request-list/RequestListItemLine";
-import { REQUEST_ITEM_SPACE_SIZE } from "@/constant/request-response.constant";
+import RequestListItemFolderEmpty from "@/components/app/collections/request-list/content/request-list/RequestListItemFolderEmpty";
 
 interface Props extends Pick<RequestListItemInterface, "children"> {
   id: string;
@@ -13,39 +8,10 @@ interface Props extends Pick<RequestListItemInterface, "children"> {
 }
 
 const RequestListItemExpendedContent = ({ id, children, lavel }: Props) => {
-  const dispatch = useAppDispatch();
-  const handleAddRequest = useCallback(
-    async () => await dispatch(createSingleRequest(id)),
-    [dispatch, id]
-  );
-
-  const emptyLeftSpace =
-    REQUEST_ITEM_SPACE_SIZE * (lavel + 1) +
-    4; /* 4 for aligning with extra padding */
-
   return (
     <div className="w-full select-text cursor-text">
       {!Array.isArray(children) || !children.length ? (
-        <div
-          className="text-xs text-muted-foreground"
-          style={{
-            paddingLeft: emptyLeftSpace,
-          }}
-        >
-          <RequestListItemLine lavel={lavel + 1} className="px-1 select-none">
-            This folder is empty.
-            <br />
-            <Button
-              variant={"link"}
-              size={"sm"}
-              className="px-0 text-xs!"
-              onClick={handleAddRequest}
-            >
-              Add a request
-            </Button>{" "}
-            to start working.
-          </RequestListItemLine>
-        </div>
+        <RequestListItemFolderEmpty id={id} lavel={lavel + 1} />
       ) : (
         <>
           {children.map((id, index) => (
---
+++ src/ui/components/app/collections/request-list/content/request-list/RequestListItemFolderEmpty.tsx
@@ -0,0 +1,52 @@
+import { memo, useCallback } from "react";
+import { REQUEST_ITEM_SPACE_SIZE } from "@/constant/request-response.constant";
+import RequestListItemLine from "@/components/app/collections/request-list/content/request-list/RequestListItemLine";
+import RequestListItemContentWrapperParent from "@/components/app/collections/request-list/content/request-list/RequestListItemContentWrapperParent";
+import { Button } from "@/components/ui/button";
+import { useAppDispatch } from "@/context/redux/hooks";
+import { createSingleRequest } from "@/context/redux/request-response/thunks/request-list";
+
+interface Props {
+  id: string;
+  lavel: number;
+}
+
+const RequestListItemFolderEmpty = memo(({ id, lavel }: Props) => {
+  const dispatch = useAppDispatch();
+  const handleAddRequest = useCallback(
+    async () => await dispatch(createSingleRequest(id)),
+    [dispatch, id]
+  );
+
+  const emptyLeftSpace = REQUEST_ITEM_SPACE_SIZE * lavel;
+
+  return (
+    <RequestListItemContentWrapperParent className="h-auto cursor-auto">
+      <div
+        className="text-xs text-muted-foreground"
+        style={{
+          paddingLeft: emptyLeftSpace,
+        }}
+      >
+        <RequestListItemLine
+          lavel={lavel}
+          className="select-none leading-relaxed py-1.5"
+        >
+          This folder is empty.
+          <br />
+          <Button
+            variant={"link"}
+            size={"sm"}
+            className="px-0 text-xs! h-auto"
+            onClick={handleAddRequest}
+          >
+            Add a request
+          </Button>{" "}
+          to start working.
+        </RequestListItemLine>
+      </div>
+    </RequestListItemContentWrapperParent>
+  );
+});
+
+export default RequestListItemFolderEmpty;
