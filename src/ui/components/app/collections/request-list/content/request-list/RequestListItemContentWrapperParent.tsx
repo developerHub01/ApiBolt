68b5b2396ce712f211f3f94761182f590eb746f7
developerHub01 <developerhubytchannel@gmail.com>
2025-09-13 02:32:53 +0600

    RequestListItem style updated

--- src/ui/components/app/collections/request-list/content/request-list/RequestListItemContentWrapperParent.tsx
+++ src/ui/components/app/collections/request-list/content/request-list/RequestListItemContentWrapperParent.tsx
@@ -15,7 +15,7 @@ const RequestListItemContentWrapperParent = memo(
     return (
       <div
         className={cn(
-          "w-full cursor-pointer hover:bg-accent/50 focus-within:bg-accent/50 duration-100 transition-all px-1 border-x-2 h-9 border-transparent",
+          "w-full cursor-pointer hover:bg-accent/50 focus-within:bg-accent/50 duration-100 transition-all px-2 border-x-2 h-9 border-transparent",
           className
         )}
         {...props}
