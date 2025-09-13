17e7bcdc27fd62814490e4f0a41b47ac08564914
developerHub01 <developerhubytchannel@gmail.com>
2025-09-13 21:07:23 +0600

    update: FolderTitle counter sync with typing and showing placeholder if title is empty

--- src/ui/components/app/collections/folder/FolderTitle.tsx
+++ src/ui/components/app/collections/folder/FolderTitle.tsx
@@ -9,6 +9,7 @@ import {
 import { useAppDispatch, useAppSelector } from "@/context/redux/hooks";
 import { selectRequestFolderTitle } from "@/context/redux/request-response/request-response-selector";
 import { updateFolder } from "@/context/redux/request-response/thunks/folder";
+import { cn } from "@/lib/utils";
 
 const MAX_TITLE_LENGTH = 50;
 
@@ -16,10 +17,12 @@ const FolderTitle = memo(() => {
   const dispatch = useAppDispatch();
   const title = useAppSelector(selectRequestFolderTitle) ?? "New Folder";
   const [titleState, setTitleState] = useState<string>(title);
+  const [titleLength, setTitleLength] = useState<number>(title.length ?? 0);
 
   useEffect(() => {
     if (title === titleState) return;
     setTitleState(title);
+    setTitleLength(title.length ?? 0);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [title]);
 
@@ -31,14 +34,21 @@ const FolderTitle = memo(() => {
     if (
       el.innerText.length >= MAX_TITLE_LENGTH &&
       !["Backspace", "Delete", "ArrowLeft", "ArrowRight"].includes(e.key)
-    ) {
+    )
       e.preventDefault();
-    }
+  }, []);
+
+  const handleInput = useCallback((e: React.FormEvent<HTMLHeadingElement>) => {
+    const length = e.currentTarget?.innerText.trim().length ?? 0;
+    setTitleLength(length);
+    /* this is actually for removing some extra thing that browser by default add like </br> */
+    if (!length) e.currentTarget.innerHTML = "";
   }, []);
 
   const handleBlur = useCallback(
     (e: FocusEvent<HTMLHeadingElement>) => {
       const newTitle = e.target.innerText.trim().slice(0, MAX_TITLE_LENGTH);
+      setTitleLength(newTitle.length ?? 0);
       setTitleState(newTitle);
 
       dispatch(
@@ -56,13 +66,18 @@ const FolderTitle = memo(() => {
         contentEditable
         suppressContentEditableWarning
         onKeyDown={handleKeydown}
+        onInput={handleInput}
         onBlur={handleBlur}
-        className="w-full text-xl md:text-3xl font-semibold hover:bg-accent/50 focus:bg-accent/50 py-1 px-2 rounded-t-sm border-b outline-none"
+        className={cn(
+          "relative w-full text-xl md:text-3xl font-semibold  bg-accent/50 focus:bg-accent/50 py-1 px-2 rounded-t-sm border-b outline-none",
+          "empty:before:content-[attr(data-placeholder)] empty:before:text-gray-400 empty:before:absolute empty:before:pointer-events-none"
+        )}
+        data-placeholder="Enter title..."
       >
         {titleState}
       </h2>
       <p className="text-xs text-muted-foreground px-2">
-        {titleState.length}/{MAX_TITLE_LENGTH}
+        {titleLength}/{MAX_TITLE_LENGTH}
       </p>
     </div>
   );
