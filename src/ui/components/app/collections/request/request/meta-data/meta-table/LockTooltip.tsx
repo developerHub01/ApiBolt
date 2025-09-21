310ad105f3ac4c4e38bbd36bb5f35cb887de974c
developerHub01 <developerhubytchannel@gmail.com>
2025-09-21 06:46:18 +0600

    style: LockTooltip style updated

--- src/ui/components/app/collections/request/request/meta-data/meta-table/LockTooltip.tsx
+++ src/ui/components/app/collections/request/request/meta-data/meta-table/LockTooltip.tsx
@@ -1,11 +1,10 @@
 import { memo } from "react";
-
 import { Lock as LockIcon } from "lucide-react";
 
 const LockTooltip = memo(() => {
   return (
-    <span className="aspect-square rounded-full grid place-items-center p-1 bg-accent/50 hover:bg-accent">
-      <LockIcon size={12} />
+    <span className="aspect-square rounded-full grid place-items-center p-1.5 bg-accent/50 hover:bg-accent">
+      <LockIcon size={14} />
     </span>
   );
 });
