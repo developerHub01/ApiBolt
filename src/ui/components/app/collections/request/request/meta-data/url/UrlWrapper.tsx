c7c34bbd78f2af98950b8b391b42c0e07d0f37ac
Shakil <developerhubytchannel@gmail.com>
2025-09-29 22:52:45 +0600

    UrlWrapper layout minor update

--- src/ui/components/app/collections/request/request/meta-data/url/UrlWrapper.tsx
+++ src/ui/components/app/collections/request/request/meta-data/url/UrlWrapper.tsx
@@ -9,14 +9,14 @@ interface Props {
 
 const UrlWrapper = memo(({ className = "", children }: Props) => {
   return (
-    <div className="h-full flex flex-col gap-3 justify-center">
+    <>
       <p className="text-foreground text-sm select-none">Url</p>
       <ScrollArea className="flex-1 w-full min-h-0 h-full [&>div>div]:h-full">
         <div className={cn("w-full flex flex-wrap gap-2 pt-1 pb-5", className)}>
           {children}
         </div>
       </ScrollArea>
-    </div>
+    </>
   );
 });
 
