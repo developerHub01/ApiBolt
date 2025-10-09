a5337400157acc1798cb076b1e8c41d4643e4411
Shakil <developerhubytchannel@gmail.com>
2025-10-09 22:24:28 +0600

    fix: selectParsedRequestUrl now also support authorizationHiddenParams when it  is addTo param type

--- src/ui/context/redux/request-url/selectors/url.ts
+++ src/ui/context/redux/request-url/selectors/url.ts
@@ -28,16 +28,29 @@ export const selectRequestUrl = createSelector(
 );
 
 export const selectParsedRequestUrl = createSelector(
+  (state: RootState) => state.requestResponse.selectedTab,
   (state: RootState) =>
     state.requestUrl.tokens[state.requestResponse.selectedTab ?? ""],
   (state: RootState) =>
     state.requestResponse.params[state.requestResponse.selectedTab ?? ""],
+  (state: RootState) => state.requestResponse.authorizationParam,
+  (state: RootState) =>
+    state.requestResponse.authType[state.requestResponse.selectedTab ?? ""],
+  (state: RootState) =>
+    state.requestResponse.authInheritedId[
+      state.requestResponse.selectedTab ?? ""
+    ],
   (state: RootState) => state.requestResponse.environmentsList,
   (
+    selectedTab,
     tokens: Array<UrlTokenInterface>,
     params: Array<ParamInterface>,
+    authorizationParams,
+    authType,
+    authInheritedId,
     environmentsList
   ): string => {
+    const paramsCopy = [...(params ?? [])];
     const envMap = removeDuplicateEnvs(environmentsList);
     tokens = tokens ?? [...INITIAL_URL_TOKENS_VALUE];
 
@@ -51,7 +64,14 @@ export const selectParsedRequestUrl = createSelector(
       };
     });
 
-    const queryParams = paramsTableToStringParsedFromEnvs(params, envMap);
+    /* here authHiddenParam will appply based on the authType if authType is anything but inherited-parents then use selectedTab hiddenParam else that inerited-parents meta request hiddenParams */
+    const authirizationParam =
+      authType === "inherit-parent"
+        ? authorizationParams[authInheritedId!]
+        : authorizationParams[selectedTab!];
+    if (authirizationParam) paramsCopy.push(authirizationParam);
+
+    const queryParams = paramsTableToStringParsedFromEnvs(paramsCopy, envMap);
     try {
       return decodeApiUrl(tokens) + queryParams;
     } catch (error) {
