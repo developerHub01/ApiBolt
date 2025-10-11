10afa1087a2ef97519bf305820a890c607b246a5
Shakil <developerhubytchannel@gmail.com>
2025-10-11 07:38:35 +0600

    feat & update generateGoFasthttpCode created and generateGoNetHttpCode updated, refactor and bug fixed and go reference markdown file added

--- src/ui/utils/snippet-generator/go/go.utils.ts
+++ src/ui/utils/snippet-generator/go/go.utils.ts
@@ -26,20 +26,10 @@ export const generateGoNetHttpCode = async ({
   bodyType,
   formData,
 }: CodeSnippitDataInterface) => {
-  const codeStartString = `package main
+  const packageString = `package main\n\n`;
+  const imports: Array<string> = ["fmt", "net/http", "net/url", "io"];
 
-import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"io"
-	"mime/multipart"
-	"net/http"
-	"net/url"
-	"os"
-)
-
-func main() {
+  const codeStartString = `func main() {
 \turlStr := "${url}"\n\n`;
 
   /* ============== FORM DATA ================== */
@@ -77,7 +67,7 @@ func main() {
   \twriter.Close()\n\n`;
 
     formDataString = formData.length
-      ? `\t// ================= form-data =================
+      ? `\t// ================= FORM DATA =================
   ${formDataReadingString}`
       : "";
   }
@@ -89,15 +79,15 @@ func main() {
     xWWWFormUrlencoded.length &&
     method.toLowerCase() !== "get"
   ) {
-    xWWWFormUrlencodedString = `\t// ================= x-www-form-urlencoded =================
+    xWWWFormUrlencodedString = `\t// ================= XWWWFORMURLENCODED DATA =================
 \tdata := url.Values{}
 ${xWWWFormUrlencoded.map(({ key, value }) => `\tdata.Set(${JSON.stringify(key)}, ${JSON.stringify(value)})`).join("\n")}\n\n`;
   }
 
   let binaryDataString = "";
   if (bodyType === "binary" && method.toLowerCase() !== "get") {
-    binaryDataString = `\t// ================= binary =================
-\tbinData, _ := os.ReadFile(${binaryData ? JSON.stringify(binaryData) : "/your_file_path"})\n\n`;
+    binaryDataString = `\t// ================= BINARY DATA =================
+\tbinData, _ := os.ReadFile(${binaryData ? JSON.stringify(binaryData) : JSON.stringify("/your_file_path")})\n\n`;
   }
 
   let rawDataString = "";
@@ -108,15 +98,17 @@ ${xWWWFormUrlencoded.map(({ key, value }) => `\tdata.Set(${JSON.stringify(key)},
         .split("\n")
         ?.map((line, index) => (index ? `\t${line}` : line))
         .join("\n");
-      rawDataString = `map[string]interface{}${codeString}`;
-    } else rawDataString = JSON.stringify(rawData);
+      rawDataString = `// TODO: convert JSON object to Go map if it is broken.
+\tjsonData := map[string]interface{}${codeString}
+\tdata, _ := json.Marshal(jsonData)`;
+    } else rawDataString = `data := ${JSON.stringify(rawData)}`;
 
     if (rawDataString)
-      rawDataString = `\t// ================= raw data =================\n\tdata := ${rawDataString}\n\n`;
+      rawDataString = `\t// ================= RAW DATA =================\n\t${rawDataString}\n\n`;
   }
 
-  const codeEndingString = `\tresp, _ = http.DefaultClient.Do(req)
-\tbody, _ = io.ReadAll(resp.Body)
+  const codeEndingString = `\tresp, _ := http.DefaultClient.Do(req)
+\tbody, _ := io.ReadAll(resp.Body)
 \tfmt.Println(string(body))
 \tresp.Body.Close()
 }`;
@@ -133,7 +125,7 @@ ${xWWWFormUrlencoded.map(({ key, value }) => `\tdata.Set(${JSON.stringify(key)},
   if (!requestDataString || method.toLowerCase() === "get")
     requestDataString = "nil";
 
-  const requestString = `\treq, _ = http.NewRequest("${method.toUpperCase()}", urlStr, ${requestDataString})\n`;
+  const requestString = `\treq, _ := http.NewRequest("${method.toUpperCase()}", urlStr, ${requestDataString})\n`;
 
   let contentTypeHeader = "";
   if (method.toLowerCase() !== "get") {
@@ -172,11 +164,202 @@ ${xWWWFormUrlencoded.map(({ key, value }) => `\tdata.Set(${JSON.stringify(key)},
     if (headersString) headersString += "\n\n";
   }
 
-  const code = `${codeStartString}${formDataString}${xWWWFormUrlencodedString}${binaryDataString}${rawDataString}${requestString}${contentTypeHeader}${headersString}${codeEndingString}`;
+  // Conditionally add packages based on bodyType
+  if (formDataString) imports.push("bytes", "mime/multipart", "os");
+  else if (binaryDataString) imports.push("bytes", "os");
+  else if (xWWWFormUrlencodedString) imports.push("bytes");
+  else if (rawDataString) {
+    if (rawBodyDataType === "json") imports.push("bytes", "encoding/json");
+    else imports.push("bytes");
+  }
+
+  // Generate import block
+  const importBlock = `import (\n${Array.from(new Set(imports))
+    .map((imp) => `\t"${imp}"`)
+    .join("\n")}\n)\n\n`;
+
+  const code = `${packageString}${importBlock}${codeStartString}${formDataString}${xWWWFormUrlencodedString}${binaryDataString}${rawDataString}${requestString}${contentTypeHeader}${headersString}${codeEndingString}`;
   return generateMaskedAndRealCode({ code, authorization });
 };
 
-export const generateGoFasthttpCode = () => {};
+export const generateGoFasthttpCode = async ({
+  url,
+  method,
+  headers,
+  authorization,
+  xWWWFormUrlencoded,
+  rawData,
+  binaryData,
+  rawBodyDataType,
+  bodyType,
+  formData,
+}: CodeSnippitDataInterface) => {
+  const imports: Array<string> = ["fmt"];
+  const packageString = `package main\n\n`;
+  const codeStartString = `func main() {
+\turl := "${url}"
+
+\treq := fasthttp.AcquireRequest()
+\tresp := fasthttp.AcquireResponse()
+\tdefer fasthttp.ReleaseRequest(req)
+\tdefer fasthttp.ReleaseResponse(resp)
+
+\treq.Header.SetMethod("${method.toUpperCase()}")
+\treq.SetRequestURI(url)\n\n`;
+
+  let setBodyValueString = "";
+
+  /* ============== FORM DATA ================== */
+  let formDataString = "";
+  if (
+    bodyType === "form-data" &&
+    formData.length &&
+    method.toLowerCase() !== "get"
+  ) {
+    const formDataNormalString = formData.some((form) => form.type === "text")
+      ? `${formData.map(({ key, value }) => `\twriter.WriteField(${JSON.stringify(key)}, ${JSON.stringify(value)})`).join("\n")}\n\n`
+      : "";
+
+    const formDataFilesString = formData.some((form) => form.type === "file")
+      ? `${formData
+          .map(
+            (
+              { key, value },
+              index
+            ) => `\tfile${index + 1}, _ := os.Open(${JSON.stringify(value)})
+  \tdefer file${index + 1}.Close()
+  \tpart${index + 1}, _ := writer.CreateFormFile(${JSON.stringify(key)}, ${JSON.stringify(value)})
+  \tio.Copy(part${index + 1}, file${index + 1})`
+          )
+          .join("\n\n")}\n`
+      : "";
+
+    let formDataReadingString =
+      formDataNormalString.concat(formDataFilesString);
+
+    if (formDataReadingString)
+      formDataReadingString = `\t// Create multipart writer
+\tbody := &bytes.Buffer{}
+\twriter := multipart.NewWriter(body)
+${formDataReadingString}
+\twriter.Close()\n\n`;
+
+    formDataString = formData.length
+      ? `\t// ================= FORM-DATA =================
+  ${formDataReadingString}`
+      : "";
+
+    setBodyValueString = "body.Bytes()";
+  }
+
+  /* ============== XWWWFORMURLENCODED DATA ================== */
+  let xWWWFormUrlencodedString = "";
+  if (
+    bodyType === "x-www-form-urlencoded" &&
+    xWWWFormUrlencoded.length &&
+    method.toLowerCase() !== "get"
+  ) {
+    const paramsString = xWWWFormUrlencoded
+      .map(({ key, value }) => `${key}=${value}`)
+      .join("&");
+
+    xWWWFormUrlencodedString = `\t// ================= X-WWW-FORM-URLENCODED =================
+\tdata := []byte(${JSON.stringify(paramsString)})\n\n`;
+
+    setBodyValueString = "data";
+  }
+
+  let binaryDataString = "";
+  if (bodyType === "binary" && method.toLowerCase() !== "get") {
+    binaryDataString = `\t// ================= binary =================
+\tbinData, _ := os.ReadFile(${binaryData ? JSON.stringify(binaryData) : "/your_file_path"})\n\n`;
+
+    setBodyValueString = "binData";
+  }
+
+  let rawDataString = "";
+  /* ============== RAW DATA ================== */
+  if (bodyType === "raw" && method.toLowerCase() !== "get") {
+    if (rawBodyDataType === "json" && isStringIsValidObject(rawData)) {
+      const codeString = `${await jsonFormatter(rawData)}`
+        .split("\n")
+        ?.map((line, index) => (index ? `\t${line}` : line))
+        .join("\n");
+      rawDataString = `// TODO: convert JSON object to Go map if it is broken.
+\tjsonData := map[string]interface{}${codeString}
+\tdata, _ := json.Marshal(jsonData)`;
+      setBodyValueString = "data";
+    } else {
+      rawDataString = `data := ${JSON.stringify(rawData)}`;
+      setBodyValueString = "[]byte(data)";
+    }
+
+    rawDataString = `\t// ================= raw data =================\n\t${rawDataString}\n\n`;
+  }
+
+  let contentTypeHeader = "";
+  if (method.toLowerCase() !== "get") {
+    if (bodyType === "x-www-form-urlencoded")
+      contentTypeHeader = `req.Header.Set("Content-Type", "application/x-www-form-urlencoded")`;
+    else if (bodyType === "form-data")
+      contentTypeHeader = `req.Header.Set("Content-Type", writer.FormDataContentType())`;
+    else if (bodyType === "raw") {
+      const headerContentType = getBodyType({
+        bodyType,
+        rawBodyDataType,
+      });
+      if (headerContentType)
+        contentTypeHeader = `req.Header.Set("Content-Type", ${JSON.stringify(headerContentType)})`;
+    }
+
+    if (contentTypeHeader) contentTypeHeader = `\t${contentTypeHeader}\n`;
+  }
+
+  const setBodyString = setBodyValueString
+    ? `\treq.SetBody(${setBodyValueString})\n`
+    : "";
+
+  let headersString = "";
+  if (headers.length || authorization) {
+    if (authorization)
+      headers = [
+        {
+          ...authorization,
+          value: MASKED_AUTHORIZATION,
+        },
+        ...headers,
+      ];
+    headersString = headers
+      .map(
+        ({ key, value }) =>
+          `\treq.Header.Set(${JSON.stringify(key)}, ${JSON.stringify(value)})`
+      )
+      .join("\n");
+    if (headersString) headersString += "\n\n";
+  }
+
+  // Only include packages if they are actually used
+  if (formDataString || xWWWFormUrlencodedString || binaryDataString) {
+    imports.push("bytes", "io", "mime/multipart", "os");
+  }
+
+  imports.push("", "github.com/valyala/fasthttp");
+
+  const importBlock = `import (\n${imports.map((imp) => (imp ? `\t"${imp}"` : "")).join("\n")}\n)\n\n`;
+
+  const codeEndingString = `\t// ================= Send =================
+\terr := fasthttp.Do(req, resp)
+\tif err != nil {
+\t\tfmt.Println("Request failed:", err)
+\t\treturn
+\t}
+
+\tfmt.Println(string(resp.Body()))
+}`;
+
+  const code = `${packageString}${importBlock}${codeStartString}${formDataString}${xWWWFormUrlencodedString}${binaryDataString}${rawDataString}${contentTypeHeader}${setBodyString}${headersString}${codeEndingString}`;
+  return generateMaskedAndRealCode({ code, authorization });
+};
 
 export const generateGoCode = async (
   type: TRequestCodeType,
@@ -185,8 +368,8 @@ export const generateGoCode = async (
   switch (type) {
     case "go-net-http":
       return await generateGoNetHttpCode(data);
-    // case "go-fasthttp":
-    //   return await generateGoFasthttpCode();
+    case "go-fasthttp":
+      return await generateGoFasthttpCode(data);
   }
   return requestDefaultCodeSnippit;
 };
--- src/ui/utils/snippet-generator/go/readme.md
+++ src/ui/utils/snippet-generator/go/readme.md
@@ -101,9 +101,104 @@ func main() {
 	req.Header.Set("Content-Type", writer.FormDataContentType())
 	req.Header.Set("Authorization", "Bearer sdfdsfsdf")
 
-	resp, _ = http.DefaultClient.Do(req)
-	body, _ = io.ReadAll(resp.Body)
+	resp, _ := http.DefaultClient.Do(req)
+	body, _ := io.ReadAll(resp.Body)
 	fmt.Println(string(body))
 	resp.Body.Close()
 }
+```
+
+## fasthttp
+
+```go
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"os"
+
+	"github.com/valyala/fasthttp"
+)
+
+func main() {
+	url := "https://dummyjson.com/comments"
+
+	req := fasthttp.AcquireRequest()
+	resp := fasthttp.AcquireResponse()
+	defer fasthttp.ReleaseRequest(req)
+	defer fasthttp.ReleaseResponse(resp)
+
+	req.Header.SetMethod("POST")
+	req.SetRequestURI(url)
+	req.Header.Set("Authorization", "Bearer sdfdsfsdf")
+	req.Header.Set("asdfsdf", "ssddd")
+
+	// ========= Example body types =========
+
+	// ----- 1. form-data -----
+	/*
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	writer.WriteField("key1", "value1")
+
+	file, _ := os.Open("file.jpeg")
+	defer file.Close()
+	part, _ := writer.CreateFormFile("upload", "file.jpeg")
+	io.Copy(part, file)
+	writer.Close()
+
+	req.SetBody(body.Bytes())
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	*/
+
+	// ----- 2. x-www-form-urlencoded -----
+	/*
+	form := []byte("a=b&c=d")
+	req.SetBody(form)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	*/
+
+	// ----- 3. binary -----
+	/*
+	binData, _ := os.ReadFile("file.jpeg")
+	req.SetBody(binData)
+	req.Header.Set("Content-Type", "application/octet-stream")
+	*/
+
+	// ----- 4. raw text / xml / js -----
+	/*
+	raw := `let x = { "a": 1, "b": 2 }`
+	req.SetBody([]byte(raw))
+	req.Header.Set("Content-Type", "application/javascript")
+	*/
+
+	// ----- 5. json -----
+	/*
+	jsonBody := map[string]interface{}{
+		"config": map[string]interface{}{
+			"forge": map[string]interface{}{
+				"makers": []interface{}{
+					map[string]interface{}{"name": "@electron-forge/maker-zip"},
+				},
+			},
+		},
+	}
+	jsonBytes, _ := json.Marshal(jsonBody)
+	req.SetBody(jsonBytes)
+	req.Header.Set("Content-Type", "application/json")
+	*/
+
+	// ========= Send =========
+	if err := fasthttp.Do(req, resp); err != nil {
+		fmt.Println("Request failed:", err)
+		return
+	}
+
+	fmt.Println(string(resp.Body()))
+}
+
 ```
