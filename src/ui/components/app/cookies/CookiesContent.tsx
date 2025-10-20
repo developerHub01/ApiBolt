65a0a6e51affb55148409b4fce0944f46045478a
Shakil <developerhubytchannel@gmail.com>
2025-10-20 22:20:39 +0600

    feat: NoCookies update rendering condition and UI/UX

--- src/ui/components/app/cookies/CookiesContent.tsx
+++ src/ui/components/app/cookies/CookiesContent.tsx
@@ -2,23 +2,19 @@ import { memo } from "react";
 import CookiesList from "@/components/app/cookies/cookies-list/CookiesList";
 import CookieDetails from "@/components/app/cookies/cookie-details/CookieDetails";
 import NoCookies from "@/components/app/cookies/cookies-list/NoCookies";
-import {
-  selectCookiesCount,
-  selectIsAddOptionOpen,
-} from "@/context/redux/cookies/selectors/cookies-selector";
+import { selectCookiesCount } from "@/context/redux/cookies/selectors/cookies-selector";
 import { useAppSelector } from "@/context/redux/hooks";
 import AddCookieDetails from "@/components/app/cookies/add-cookie/AddCookieDetails";
 
 const CookiesContent = memo(() => {
   const cookiesCount = useAppSelector(selectCookiesCount);
-  const isAddOptionOpen = useAppSelector(selectIsAddOptionOpen);
 
   return (
     <div className="w-full h-full flex flex-col gap-4 p-2">
       <CookiesList />
       <AddCookieDetails />
       {Boolean(cookiesCount) && <CookieDetails />}
-      {!cookiesCount && !isAddOptionOpen && <NoCookies />}
+      <NoCookies />
     </div>
   );
 });
--- src/ui/components/app/cookies/cookies-list/NoCookies.tsx
+++ src/ui/components/app/cookies/cookies-list/NoCookies.tsx
@@ -1,11 +1,27 @@
 import Empty from "@/components/ui/empty";
+import {
+  selectCookiesCount,
+  selectIsAddOptionOpen,
+  selectSelectedCookieKey,
+} from "@/context/redux/cookies/selectors/cookies-selector";
+import { useAppSelector } from "@/context/redux/hooks";
 
 const NoCookies = () => {
+  const cookiesCount = useAppSelector(selectCookiesCount);
+  const isAddOptionOpen = useAppSelector(selectIsAddOptionOpen);
+  const selectedCookieKey = useAppSelector(selectSelectedCookieKey);
+
+  if (isAddOptionOpen || selectedCookieKey) return null;
+
   return (
     <Empty
-      label="No Cookies"
-      description="It seems like, no cookies saved in project."
-      animationSrc="./lottie/nodata.lottie"
+      label={cookiesCount ? "Cookies" : "No Cookies"}
+      description={
+        cookiesCount
+          ? "Showing cookies saved in the project"
+          : "It seems like, no cookies saved in the project."
+      }
+      animationSrc="./lottie/cookie.json"
       showFallback
       innerClassName="w-56"
       className="h-full"
