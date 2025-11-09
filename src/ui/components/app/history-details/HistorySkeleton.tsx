b7d9450b6b7e07e6137ceff4523378cbe8d37d07
Shakil <developerhubytchannel@gmail.com>
2025-11-09 14:27:19 +0600

    feat: HistorySkeleton created

--- src/ui/components/app/history-details/HistorySkeleton.tsx
+++ src/ui/components/app/history-details/HistorySkeleton.tsx
@@ -1,7 +1,33 @@
 import { memo } from "react";
+import {
+  AnimatedDialogContent,
+  AnimatedDialogContentScroll,
+} from "@/components/ui/animated-dialog";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const HistorySkeleton = memo(() => {
-  return <div>HistorySkeleton</div>;
+  return (
+    <AnimatedDialogContent>
+      <AnimatedDialogContentScroll>
+        <div className="h-full flex flex-col gap-3">
+          {/* skeleton top */}
+          <div className="h-9 flex items-center gap-2">
+            {Array.from({ length: 4 }, (_, index) => (
+              <Skeleton key={index} className="w-20 h-full" />
+            ))}
+          </div>
+          {/* skeleton content */}
+          <Skeleton className="w-full flex-1" />
+          {/* skeleton bottom */}
+          <div className="h-9 w-full flex gap-2">
+            <Skeleton className="w-20 mr-auto" />
+            <Skeleton className="w-20" />
+            <Skeleton className="w-20" />
+          </div>
+        </div>
+      </AnimatedDialogContentScroll>
+    </AnimatedDialogContent>
+  );
 });
 
 export default HistorySkeleton;
