f27be63ca233a639a1a461008b4e4f54dab96697
Shakil <developerhubytchannel@gmail.com>
2025-11-18 04:43:31 +0600

    feat: update sidebar toggler logic to show based on active tab and location

--- src/ui/hooks/sidebar/use-show-sidebar-toggler.tsx
+++ src/ui/hooks/sidebar/use-show-sidebar-toggler.tsx
@@ -1,10 +1,19 @@
-import { SIDEBAR_TOGGLE_BUTTON_ALLOWED_IDS } from "@/constant/sidebar.constant";
 import { useAppSelector } from "@/context/redux/hooks";
 import { selectSidebarActiveTab } from "@/context/redux/sidebar/selectors/sidebar";
+import { useLocation } from "react-router-dom";
 
 const useShowSidebarToggler = () => {
   const activeTab = useAppSelector(selectSidebarActiveTab);
-  return SIDEBAR_TOGGLE_BUTTON_ALLOWED_IDS.has(activeTab);
+  const location = useLocation();
+
+  if (
+    activeTab === "navigate_collections" ||
+    (activeTab === "navigate_themes" &&
+      location.pathname === "/themes/marketplace")
+  )
+    return true;
+
+  return false;
 };
 
 export default useShowSidebarToggler;
