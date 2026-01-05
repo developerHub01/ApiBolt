2cba37e4533cfd52cb77e99920e51ce4baa24f8f
Shakil <developerhubytchannel@gmail.com>
2026-01-06 05:38:37 +0600

    format

--- src/main/utils/splashWindow.ts
+++ src/main/utils/splashWindow.ts
@@ -31,7 +31,7 @@ export const createSplashWindow = () => {
     shell.openExternal(details.url);
     return { action: "deny" };
   });
-  
+
   // HMR for renderer base on electron-vite cli.
   // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env["ELECTRON_RENDERER_URL"]) {
