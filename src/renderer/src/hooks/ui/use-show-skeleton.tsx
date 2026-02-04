e86f202254847e431552fff8d0e54a6241adc4ac
developerHub01 <shakil102043@gmail.com>
2026-02-04 20:36:47 +0600

    optimized useShowSkeleton by removing unnecessary useEffect by using recommended way of react19 comparing state.

--- src/renderer/src/hooks/ui/use-show-skeleton.tsx
+++ src/renderer/src/hooks/ui/use-show-skeleton.tsx
@@ -2,20 +2,20 @@ import { useEffect, useState } from "react";
 
 const useShowSkeleton = (isLoading: boolean, delay = 300) => {
   const [showLoading, setShowLoading] = useState<boolean>(false);
+  const [prevIsLoading, setPrevIsLoading] = useState<boolean>(isLoading);
 
   useEffect(() => {
-    let timer: NodeJS.Timeout;
-
-    if (isLoading) {
-      // Immediately start showing skeleton
-      setShowLoading(true);
-    } else {
-      // Delay hiding to avoid the flash
-      timer = setTimeout(() => setShowLoading(false), delay);
+    if (!isLoading) {
+      const timer = setTimeout(() => setShowLoading(false), delay);
+      return () => clearTimeout(timer);
     }
-
-    return () => clearTimeout(timer);
+    return;
   }, [isLoading, delay]);
+
+  if (isLoading !== prevIsLoading) {
+    setPrevIsLoading(isLoading);
+    if (isLoading) setShowLoading(true);
+  }
 
   return showLoading;
 };
